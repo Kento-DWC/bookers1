7953f3fefca42fd2b549b93ca1e18b5d4d1e6f04
Kento-DWC <tada.kento.infratop@gmail.com>
2022-01-11 12:25:15 +0000

    機能完成、テスト完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,15 +10,38 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.delete
+    redirect_to books_path
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
